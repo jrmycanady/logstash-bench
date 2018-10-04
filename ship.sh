969770634c806e460288a6c0cf6256e6a7208c45
Jeremy Canady <jcanady@gmail.com>
2018-10-04 13:02:13 -0500

    removes useless file

--- ship.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-env GOOS=linux go build -o logstash-bench-linux-x64
-env GOOS=darwin go build -o logstash-bin-darwin-x64
-
-scp ./logstash-bench-linux-x64 root@jcdesktop.inf.labs/logstashtest
